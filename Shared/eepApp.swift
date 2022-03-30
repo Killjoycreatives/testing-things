e2d4089dfa3007a71f9844dda118d1f380eab215
Madot <80711263+Killjoycreatives@users.noreply.github.com>
2022-03-29 20:43:18 -0400

    Initial Commit

---
+++ Shared/eepApp.swift
@@ -0,0 +1,17 @@
+//
+//  eepApp.swift
+//  Shared
+//
+//  Created by Madot on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct eepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
